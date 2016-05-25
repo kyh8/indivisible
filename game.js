ca3e894122fe0eac91db8629f6b71483207ef955
Kevin He <kyh8@duke.edu>
2016-05-24 22:00:59 -0700

    Falling block

---
+++ game.js
@@ -0,0 +1,18 @@
+function runGame(){
+  var block = document.getElementById("block-1");
+  var position = block.style.top;
+  position = parseInt(position.substring(0,position.length-2));
+  console.log(position);
+  if(position >= 600){
+    block.parentNode.removeChild(block);
+  }
+  block.style.top = position + 1 + "px";
+
+  setTimeout(function(){
+    runGame();
+  }, 10);
+}
+
+$(document).ready(function(){
+  runGame();
+});
