396fdfa749d489cc59fe8067b1521a8d0fc7c3bf
Kevin He <kyh8@duke.edu>
2016-05-26 22:33:59 -0700

    Lose condition added

--- game.js
+++ game.js
@@ -12,7 +12,8 @@ var MOVE_SPEED = 1;
 var MOVE_REFRESH = 3;
 var MIN_GEN_RATE = 1;
 var MAX_GEN_RATE = 3;
-var LEVEL_GEN_NUM = 20; //blocks per level
+var LEVEL_GEN_NUM = 10; //blocks per level
+var GAME_BOARD_HEIGHT = 500;
 
 var gameRactive;
 
@@ -22,27 +23,29 @@ function runGame(){
 }
 
 function runLevel(spawns, i){
+  if(gameRactive.get('lost')){
+    return;
+  }
   if(i == spawns.length){
     console.log('NEW LEVEL!');
-    gameRactive.set('divisor', gameRactive.get('divisor') + 1);
-    console.log('new divisor: ' + gameRactive.get('divisor'));
+    dropLine();
     var spawnTimes = calcSpawns();
     runLevel(spawnTimes, 0);
     return;
   }
   setTimeout(function(){
-    console.log("SPAWN INDEX: " + i);
+    // console.log("SPAWN INDEX: " + i);
     var blocks = gameRactive.get('blocks');
     var blockIndex = gameRactive.get('blocknum');
     blocks.push({
       'id': blockIndex,
       'value': 1
     });
-    console.log(blocks);
+    // console.log(blocks);
     // console.log('index: ' + blockIndex);
     gameRactive.set('blocknum', blockIndex+1);
     var blockElement = document.getElementById("block-"+blockIndex);
-    var offset = Math.floor(Math.random() * 480);
+    var offset = Math.floor(Math.random() * 460);
     blockElement.style.left = offset + "px";
 
     dropBlock(blockIndex);
@@ -63,6 +66,10 @@ function calcSpawns(){
 }
 
 function checkCollisions(){
+  if(gameRactive.get('lost')){
+    return;
+  }
+
   var blocks = gameRactive.get('blocks');
   var gameBlocks = [];
   for(var i = 0; i < blocks.length; i++){
@@ -87,8 +94,8 @@ function checkCollisions(){
       // console.log(blockRect);
       // console.log(playerRect);
       // console.log('checking');
-      if((bl < pr && bl > pl || br < pr && br > pl) &&
-          (bt > pt && bt < pb || bb > pt && bb < pb)){
+      if((pl < br && pl > bl || pr < br && pr > bl) &&
+          (pt > bt && pt < bb || pb > bt && pb < bb)){
         // console.log("COLLIDE");
         // $('#block-1').fadeOut(100);
         gameRactive.set('score', gameRactive.get('score') + 1);
@@ -106,6 +113,47 @@ function checkCollisions(){
   }, COLLISION_REFRESH);
 }
 
+function dropLine(){
+  // console.log('line dropping');
+  var levelLine = document.getElementById("level");
+  var position = levelLine.style.top;
+  position = parseInt(position.substring(0,position.length-2));
+  console.log('position of level line: ' + position);
+
+  var level = levelLine.getBoundingClientRect();
+  var bottom = level.bottom;
+
+  var player = document.getElementById("player-block");
+  var playerBlock = player.getBoundingClientRect();
+  var playerTop = playerBlock.top;
+  var playerBottom = playerBlock.bottom;
+
+  if(position >= GAME_BOARD_HEIGHT){
+    levelLine.style.top = '-12px';
+    gameRactive.set('levelPassed', false);
+    levelLine.style.backgroundColor = 'red';
+    return;
+  }
+  else{
+    levelLine.style.top = position + 1 + "px";
+    if(bottom >= playerTop && bottom <= playerBottom && !gameRactive.get('levelPassed')){
+      if(gameRactive.get('score') % gameRactive.get('divisor') == 0){
+        gameRactive.set('levelPassed', true);
+        gameRactive.set('divisor', gameRactive.get('divisor') + 1);
+        console.log('new divisor: ' + gameRactive.get('divisor'));
+        levelLine.style.backgroundColor = 'green';
+      }
+      else{
+        gameRactive.set('lost', true);
+      }
+    }
+  }
+
+  setTimeout(function(){
+    dropLine();
+  }, 7);
+}
+
 function dropBlock(index){
   var block = document.getElementById("block-" + index);
   // console.log(index);
@@ -113,7 +161,7 @@ function dropBlock(index){
     var position = block.style.top;
     position = parseInt(position.substring(0,position.length-2));
     // console.log(position);
-    if(position >= 600){
+    if(position >= GAME_BOARD_HEIGHT){
       var blocks = gameRactive.get('blocks');
       var blockIndex = -1;
       for(var i = 0; i < blocks.length; i++){
@@ -135,6 +183,9 @@ function dropBlock(index){
 }
 
 function handleMotion(){
+  if(gameRactive.get('lost')){
+    return;
+  }
   var player = document.getElementById("player-block");
   var position = player.style.left;
   position = parseInt(position.substring(0,position.length-2));
@@ -161,7 +212,9 @@ $(document).ready(function(){
       score: 0,
       divisor: 2,
       blocks: [],
-      blocknum: 0
+      blocknum: 0,
+      lost: false,
+      levelPassed: false
     }
   });
 
@@ -184,14 +237,36 @@ $(document).ready(function(){
     // console.log('ractive:' + gameRactive.get('keydown'));
   });
 
+  $("#retry").hover(function(){
+      document.getElementById('retry-image').width = '90';
+      document.getElementById('retry-image').height = '90';
+      document.getElementById('retry-image').style.marginTop = '0px';
+    },
+    function(){
+      document.getElementById('retry-image').width = '70';
+      document.getElementById('retry-image').height = '70';
+      document.getElementById('retry-image').style.marginTop = '10px';
+    });
+
   gameRactive.observe('keydown', function(newValue, oldValue){
     // console.log(newValue);
     var moving = DIRECTION.hasOwnProperty(newValue);
     gameRactive.set('moving', moving);
   });
 
+  gameRactive.observe('lost', function(newValue, oldValue){
+    if(newValue){
+      console.log('YOU LOST');
+    }
+  });
+
+  gameRactive.observe('levelPassed', function(newValue, oldValue){
+    if(newValue){
+      console.log('YOU PASSED THE LEVEL');
+    }
+  });
+
   handleMotion();
-  // dropBlock();
   checkCollisions();
   runGame();
 });
